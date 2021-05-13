9acb6edb58426b4fcb41783a179454aee5cebb92
vilo4kaaa <vika.masyuk@gmail.com>
2021-05-13 17:53:44 +0300

    add possibility to turn off the client

--- src/bsu/rfe/java/group10/lab7/Masyuk/A4/MainFrame.java
+++ src/bsu/rfe/java/group10/lab7/Masyuk/A4/MainFrame.java
@@ -11,6 +11,7 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+
 import javax.swing.BorderFactory;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
@@ -29,23 +30,34 @@ import javax.swing.JRadioButton;
 import javax.swing.ButtonGroup;
 
 @SuppressWarnings("serial")
-
 public class MainFrame extends JFrame {
+
+    private boolean turn = true;
+    private ButtonGroup radioButtons = new ButtonGroup ();
     private static final String FRAME_TITLE = "Клиент мгновенных сообщений";
+
     private static final int FRAME_MINIMUM_WIDTH = 500;
     private static final int FRAME_MINIMUM_HEIGHT = 500;
+
     private static final int FROM_FIELD_DEFAULT_COLUMNS = 10;
     private static final int TO_FIELD_DEFAULT_COLUMNS = 20;
+
     private static final int INCOMING_AREA_DEFAULT_ROWS = 10;
     private static final int OUTGOING_AREA_DEFAULT_ROWS = 5;
+
     private static final int SMALL_GAP = 5;
     private static final int MEDIUM_GAP = 10;
     private static final int LARGE_GAP = 15;
+
     private static final int SERVER_PORT = 4567;
+
     private final JTextField textFieldFrom;
     private final JTextField textFieldTo;
+
     private final JTextArea textAreaIncoming;
     private final JTextArea textAreaOutgoing;
+
+    String curStringDate;
 
     public MainFrame() {
         super(FRAME_TITLE);
@@ -55,38 +67,77 @@ public class MainFrame extends JFrame {
         final Toolkit kit = Toolkit.getDefaultToolkit();
         setLocation((kit.getScreenSize().width - getWidth()) / 2,
                 (kit.getScreenSize().height - getHeight()) / 2);
-// Текстовая область для отображения полученных сообщений
+
+        // Текстовая область для отображения полученных сообщений
         textAreaIncoming = new JTextArea(INCOMING_AREA_DEFAULT_ROWS, 0);
         textAreaIncoming.setEditable(false);
-// Контейнер, обеспечивающий прокрутку текстовой области
+
+        // Контейнер, обеспечивающий прокрутку текстовой области
         final JScrollPane scrollPaneIncoming =
                 new JScrollPane(textAreaIncoming);
-// Подписи полей
+
+        // Подписи полей
         final JLabel labelFrom = new JLabel("Подпись");
         final JLabel labelTo = new JLabel("Получатель");
+
 // Поля ввода имени пользователя и адреса получателя
         textFieldFrom = new JTextField(FROM_FIELD_DEFAULT_COLUMNS);
         textFieldTo = new JTextField(TO_FIELD_DEFAULT_COLUMNS);
-// Текстовая область для ввода сообщения
+
+        // Текстовая область для ввода сообщения
         textAreaOutgoing = new JTextArea(OUTGOING_AREA_DEFAULT_ROWS, 0);
+
 // Контейнер, обеспечивающий прокрутку текстовой области
         final JScrollPane scrollPaneOutgoing =
                 new JScrollPane(textAreaOutgoing);
-// Панель ввода сообщения
+
+        // Панель ввода сообщения
         final JPanel messagePanel = new JPanel();
         messagePanel.setBorder(
                 BorderFactory.createTitledBorder("Сообщение"));
-// Кнопка отправки сообщения
-        final JButton sendButton = new JButton("Отправить");
-        sendButton.addActionListener(new ActionListener() {
-            @Override
+        
+
+        // Кнопка отправки сообщения
+        final JButton buttonSend = new JButton("Отправить");
+        buttonSend.addActionListener(new ActionListener() {
+
+
             public void actionPerformed(ActionEvent e) {
                 sendMessage();
             }
         });
-// Компоновка элементов панели "Сообщение"
+        ///////////////////////////////////////////////////////////////////////////
+        final ButtonGroup myButtons = new ButtonGroup();
+
+        JRadioButton radio1 = new JRadioButton ("Вкл.",true);
+        myButtons.add (radio1);
+        radio1.addActionListener(new ActionListener() {
+
+
+            public void actionPerformed(ActionEvent e) {
+                if(!turn){
+                    turn = true;
+                    textAreaIncoming.append("Клиент включен" + "\n");
+                    buttonSend.setEnabled(true);}
+            }
+        });
+
+        JRadioButton radio2 = new JRadioButton ("Выкл.",true);
+        myButtons.add (radio2);
+        radio2.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                if(turn){
+                    turn = false;
+                    textAreaIncoming.append("Клиент выключен" + "\n");
+                    buttonSend.setEnabled(false);}
+            }
+        });
+
+        // Компоновка элементов панели "Сообщение"
         final GroupLayout layout2 = new GroupLayout(messagePanel);
         messagePanel.setLayout(layout2);
+
         layout2.setHorizontalGroup(layout2.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout2.createParallelGroup(Alignment.TRAILING)
@@ -99,10 +150,13 @@ public class MainFrame extends JFrame {
                                 .addGap(SMALL_GAP)
                                 .addComponent(textFieldTo))
                         .addComponent(scrollPaneOutgoing)
-                        .addComponent(sendButton))
+                        .addComponent(radio1)
+                        .addComponent(radio2)
+                        .addComponent(buttonSend))
                 .addContainerGap());
         layout2.setVerticalGroup(layout2.createSequentialGroup()
                 .addContainerGap()
+
                 .addGroup(layout2.createParallelGroup(Alignment.BASELINE)
                         .addComponent(labelFrom)
                         .addComponent(textFieldFrom)
@@ -111,11 +165,15 @@ public class MainFrame extends JFrame {
                 .addGap(MEDIUM_GAP)
                 .addComponent(scrollPaneOutgoing)
                 .addGap(MEDIUM_GAP)
-                .addComponent(sendButton)
+                .addComponent(buttonSend)
+                .addComponent(radio1)
+                .addComponent(radio2)
                 .addContainerGap());
-// Компоновка элементов фрейма
+
+        // Компоновка элементов фрейма
         final GroupLayout layout1 = new GroupLayout(getContentPane());
         setLayout(layout1);
+
         layout1.setHorizontalGroup(layout1.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout1.createParallelGroup()
@@ -128,33 +186,42 @@ public class MainFrame extends JFrame {
                 .addGap(MEDIUM_GAP)
                 .addComponent(messagePanel)
                 .addContainerGap());
-// Создание и запуск потока-обработчика запросов
+
+        // Создание и запуск потока - обработчика запросов
         new Thread(new Runnable() {
-            @Override
+
+
             public void run() {
                 try {
-                    final ServerSocket serverSocket =
+//прием сооб
+                    final ServerSocket serverSocket =  //Создает серверный сокет, не привязанный к конкретному адресу
                             new ServerSocket(SERVER_PORT);
                     while (!Thread.interrupted()) {
                         final Socket socket = serverSocket.accept();
                         final DataInputStream in = new DataInputStream(
                                 socket.getInputStream());
-// Читаем имя отправителя
+
+                        // Читаем имя отправителя
                         final String senderName = in.readUTF();
-// Читаем сообщение
+
+                        // Читаем сообщение
                         final String message = in.readUTF();
-// Закрываем соединение
-                        socket.close();
-// Выделяем IP-адрес
+
+                        // Выделяем IP-адрес
                         final String address =
                                 ((InetSocketAddress) socket
                                         .getRemoteSocketAddress())
                                         .getAddress()
                                         .getHostAddress();
+
+                        // Закрываем соединение
+                        socket.close();
 // Выводим сообщение в текстовую область
-                        textAreaIncoming.append(senderName +
-                                " (" + address + "): " +
-                                message + "\n");
+
+                        if (turn==true){
+                            textAreaIncoming.append(senderName +
+                                    " (" + address + "):" + message + "\n");
+                        }
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -162,16 +229,23 @@ public class MainFrame extends JFrame {
                             "Ошибка в работе сервера", "Ошибка",
                             JOptionPane.ERROR_MESSAGE);
                 }
+
             }
         }).start();
     }
 
+    private Object newJScrollPane(JTextArea textAreaOutgoing2) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
     private void sendMessage() {
         try {
-// Получаем необходимые параметры
-            final String senderName = textFieldFrom.getText();
-            final String destinationAddress = textFieldTo.getText();
-            final String message = textAreaOutgoing.getText();
+            // Получаем необходимые параметры
+            final String senderName = textFieldFrom.getText().trim();
+            final String destinationAddress = textFieldTo.getText().trim();
+            final String message = textAreaOutgoing.getText().trim();
+
 // Убеждаемся, что поля не пустые
             if (senderName.isEmpty()) {
                 JOptionPane.showMessageDialog(this,
@@ -187,25 +261,32 @@ public class MainFrame extends JFrame {
             }
             if (message.isEmpty()) {
                 JOptionPane.showMessageDialog(this,
-                        "Введите текст сообщения", "Ошибка",
+                        " Введите текст сообщения", "Ошибка",
                         JOptionPane.ERROR_MESSAGE);
                 return;
             }
-// Создаем сокет для соединения
-            final Socket socket =
-                    new Socket(destinationAddress, SERVER_PORT);
+
+            // Создаем сокет для соединения
+            final Socket socket =  new Socket(destinationAddress, SERVER_PORT);
+
 // Открываем поток вывода данных
-            final DataOutputStream out =
-                    new DataOutputStream(socket.getOutputStream());
-// Записываем в поток имя
+            final DataOutputStream out =  new DataOutputStream(socket.getOutputStream());
+
+            // Записываем в потоки
             out.writeUTF(senderName);
-// Записываем в поток сообщение
+
+            // Записываем в поток сообщение
             out.writeUTF(message);
-// Закрываем сокет
+
+            // Закрываем сокет
             socket.close();
+
 // Помещаем сообщения в текстовую область вывода
-            textAreaIncoming.append("Я -> " + destinationAddress + ": "
-                    + message + "\n");
+            if (turn==true){
+                textAreaIncoming.append("Я -> " + destinationAddress + ": "
+                        + message + "\n");
+            }
+
 // Очищаем текстовую область ввода сообщения
             textAreaOutgoing.setText("");
         } catch (UnknownHostException e) {
@@ -223,7 +304,8 @@ public class MainFrame extends JFrame {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
-            @Override
+
+            //
             public void run() {
                 final MainFrame frame = new MainFrame();
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
